333c080684c97fdda6df8e610298f260f1b96b64
jbalasis <cs2180008@di.uoa.gr>
2022-09-13 13:53:14 +0300

    minor subrepo compatibility changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import {RouterModule, Routes} from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('../dataSpaceUI/dataSpaceUI.module').then(m => m.DataSpaceUIModule)
+    loadChildren: () => import('../dataSpaceUI/app/dataSpaceUI.module').then(m => m.DataSpaceUIModule)
   },
   {
     path: 'requests',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {DataSpaceUIModule} from "../dataSpaceUI/dataSpaceUI.module";
+import {DataSpaceUIModule} from "../dataSpaceUI/app/dataSpaceUI.module";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
-import {ReusableComponentsModule} from "../dataSpaceUI/shared/reusablecomponents/reusable-components.module";
+import {ReusableComponentsModule} from "../dataSpaceUI/app/shared/reusablecomponents/reusable-components.module";
 
 @NgModule({
   declarations: [
