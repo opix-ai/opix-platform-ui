d4213c712b4df7b3226704089270f7a4f6569ee5
Antonis Lempesis <antleb@di.uoa.gr>
2023-04-24 13:36:05 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_ENDPOINT: '/api',
+  API_ENDPOINT: '/catalogue',
   REQUESTS_ENDPOINT: '/requests-api',
   API_LOGIN: '/oauth2/authorization/opix',
   projectName: 'Opix'
